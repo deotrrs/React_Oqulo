76f11cb9ec3f419d60f0705e9e84ed9e7fbec453
ojt <djtores102@gmail.com>
2019-01-02 16:14:24 +0800

    stylesSample

---
+++ src/modules/Test.js
@@ -0,0 +1,37 @@
+import React, {Component} from 'react';
+function BoilingVerdict(props){
+    if (props.celsius >= 100) {
+      return <p>The water would boil.</p>;
+    }
+    return <p>The water would not boil.</p>;
+  }
+
+  var data = [
+    {category: "Sporting Goods", price: "$49.99", stocked: true, name: "Football"},
+    {category: "Sporting Goods", price: "$9.99", stocked: true, name: "Baseball"},
+    {category: "Sporting Goods", price: "$29.99", stocked: false, name: "Basketball"},
+    {category: "Electronics", price: "$99.99", stocked: true, name: "iPod Touch"},
+    {category: "Electronics", price: "$399.99", stocked: false, name: "iPhone 5"},
+    {category: "Electronics", price: "$199.99", stocked: true, name: "Nexus 7"}
+  ];
+  
+  class Test extends React.Component {
+    constructor(props) {
+      super(props);
+      this.handleChange = this.handleChange.bind(this);
+      this.state = {temperature: ''};
+    }
+  
+    handleChange(e) {
+      this.setState({temperature: e.target.value});
+    }
+  
+    render() {
+      const temperature = this.state.temperature;
+      return (
+         <div><pre>{JSON.stringify(data, null, 2) }</pre></div>
+      );
+    }
+  }
+  export default Test
+  
